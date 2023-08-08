13c74b5a183e3e4825c892d30c773e94a5fc7cd4
Endie <a-abdusattorov@outlook.com>
2023-08-08 13:18:37 +0500

    updated handlers

--- main.py
+++ main.py
@@ -22,32 +22,32 @@ def start(m):
 
 @bot.message_handler(is_subscribed=True,commands=['help'])
 def help(m):
-    bot.reply_to(m,"/ban — xabarga javob tariqasida ushbu xabarni yuboring va men uni guruxdan chetlataman.\n/mute sekund — xabarga javob tariqasida ushbu xabarni yuboring va men uni siz xoxlaganingizcha ovozini o'chirib turaman\n/rasm ta'rif — siz ta'riflaganingizdek rasm chizib beraman")
+    bot.reply_to(m,"/ask savol — savolingizga javob beraman\n/rasm ta'rif — siz ta'riflaganingizdek rasm chizib beraman")
 
-@bot.message_handler(is_admin=True,commands=['ban'])
-def ban(m):
-    bot.ban_chat_member(m.chat.id,m.reply_to_message.from_user.id)
-    bot.delete_message(m.chat.id,m.id)
+# @bot.message_handler(is_admin=True,commands=['ban'])
+# def ban(m):
+#     bot.ban_chat_member(m.chat.id,m.reply_to_message.from_user.id)
+#     bot.delete_message(m.chat.id,m.id)
 
-@bot.message_handler(is_admin=True,content_types=['text'],func=lambda m: m.text.startswith('/mute'))
-def mute(m):
-    s=m.text.split()
-    bot.restrict_chat_member(m.chat.id,m.reply_to_message.from_user.id,time.time()+int(s[1]) if len(s)>0 else None)
-    bot.delete_message(m.chat.id,m.id)
+# @bot.message_handler(is_admin=True,content_types=['text'],func=lambda m: m.text.startswith('/mute'))
+# def mute(m):
+#     s=m.text.split()
+#     bot.restrict_chat_member(m.chat.id,m.reply_to_message.from_user.id,time.time()+int(s[1]) if len(s)>0 else None)
+#     bot.delete_message(m.chat.id,m.id)
 
-@bot.message_handler(is_admin=True,content_types=['text'],func=lambda m: m.text.startswith('/unmute'))
-def unmute(m):
-    s=m.text.split()
-    bot.restrict_chat_member(m.chat.id,m.reply_to_message.from_user.id,time.time()+5)
-    bot.delete_message(m.chat.id,m.id)
+# @bot.message_handler(is_admin=True,content_types=['text'],func=lambda m: m.text.startswith('/unmute'))
+# def unmute(m):
+#     s=m.text.split()
+#     bot.restrict_chat_member(m.chat.id,m.reply_to_message.from_user.id,time.time()+5)
+#     bot.delete_message(m.chat.id,m.id)
 
-@bot.message_handler(is_subscribed=True,content_types=['text'],func=lambda m: m.text.startswith('/chiz'))
+@bot.message_handler(is_subscribed=True,content_types=['text'],func=lambda m: m.text.startswith('/photo'))
 def rasm(m):
     bot.send_photo(m.chat.id,photo=gen_img(' '.join(m.text.split()[1:])),reply_to_message_id=m.id)
 
-@bot.message_handler(is_subscribed=True,content_types=['text'],func=lambda m: 'milliai' in m.text.lower(),chat_types=['group','supergroup'])
+@bot.message_handler(is_subscribed=True,content_types=['text'],func=lambda m: m.text.startswith('/ask'),chat_types=['group','supergroup'])
 def rec_gr(m):
-    bot.reply_to(m,req(m.text))
+    bot.reply_to(m,req(' '.join(m.text.split()[1:])))
 
 @bot.message_handler(is_subscribed=True,content_types=['text'],chat_types=['private'])
 def rec_pr(m):
