2f3d4a3cff292de601582c268b0f249e143e929e
Endie <a-abdusattorov@outlook.com>
2023-08-08 23:09:57 +0500

    updated bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,6 @@
 from telebot import TeleBot
+from telebot import apihelper
 from vars import BOT_TOKEN
+
+apihelper.ENABLE_MIDDLEWARE = True
 bot=TeleBot(token=BOT_TOKEN,parse_mode="MARKDOWN",threaded=False)
