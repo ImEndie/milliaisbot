c0c62fcee07b2b43f7cc97f5d365bb967d90d4ee
Endie <a-abdusattorov@outlook.com>
2023-08-09 14:55:03 +0500

    commit)

--- chat.py
+++ chat.py
@@ -1,9 +1,10 @@
 import openai
 from vars import OPENAI_KEY
+import random
 from googletrans import Translator
 translator = Translator()
 
-openai.api_key = OPENAI_KEY  # Replace with your OpenAI API key
+openai.api_key = random.choice(OPENAI_KEY)  # Replace with your OpenAI API key
 
 history=[
             {"role": "system", "content": "Hi, your name is MilliAI and you are created by TRONX-STD and Wlaneakia teams."},
@@ -11,27 +12,35 @@ history=[
         ]
 
 def req(message):
-    r=translator.translate(text=message)
-    otm=history.copy()
-    otm.append({'role':'user','content':r.text})
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=otm
-    )
-    if response['choices'][0]['message']['content']:
-        history.append(otm[-1])
-        history.append(response['choices'][0]['message'])
-        r=translator.translate(text=response['choices'][0]['message']['content'],dest='uz')
-        return r.text
-    else:
-        return "Kechirasiz, savolingizni tushunmadim"
+    while True:
+        try:
+            r=translator.translate(text=message)
+            otm=history.copy()
+            otm.append({'role':'user','content':r.text})
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=otm
+            )
+            if response['choices'][0]['message']['content']:
+                history.append(otm[-1])
+                history.append(response['choices'][0]['message'])
+                r=translator.translate(text=response['choices'][0]['message']['content'],dest='uz')
+                return r.text
+            else:
+                return "Kechirasiz, savolingizni tushunmadim"
+        except:
+            openai.api_key=random.choice(OPENAI_KEY)
 
 def gen_img(t):
-    r=translator.translate(text=t)
-    response = openai.Image.create(
-        prompt=r.text,
-        n=1,
-        size="1024x1024"
-    )
-    image_url = response['data'][0]['url']
-    return image_url
+    while True:
+        try:
+            r=translator.translate(text=t)
+            response = openai.Image.create(
+                prompt=r.text,
+                n=1,
+                size="1024x1024"
+            )
+            image_url = response['data'][0]['url']
+            return image_url
+        except:
+            openai.api_key=random.choice(OPENAI_KEY)
--- vars.py
+++ vars.py
@@ -4,6 +4,6 @@ import os
 dotenv.load_dotenv()
 
 BOT_TOKEN=os.getenv('BOT_TOKEN')
-OPENAI_KEY=os.getenv('OPENAI_KEY')
+OPENAI_KEY=os.getenv('OPENAI_KEY').split('.')
 PG_URI=os.getenv('PG_URI')
 ADMINS=os.getenv('ADMINS').split(':')
