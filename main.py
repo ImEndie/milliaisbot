29d5eb7ccf33b5ee993f7d28713c5e238fdce5b7
Endie <a-abdusattorov@outlook.com>
2023-08-24 17:35:37 +0500

    updated

--- main.py
+++ main.py
@@ -89,7 +89,7 @@ def check(m: Message):
             bot.delete_message(m.chat.id,m.id)
         except Exception as e:
             print(e)
-        bot.send_message(m.chat.id,f"Assalomu alaykum {m.from_user.first_name}, @milliaibot dan foydalanishdan oldin bizning rasmiy telegram sahifamizga va homiy telegram kanaliga obuna bo'ling. Soʻng botni qayta boshlashni unutmang!",reply_markup=keyboards.getChannelButton())
+        bot.send_message(m.chat.id,f"Assalomu alaykum {m.from_user.first_name}!\n⚠️ @milliaibot dan foydalanishdan oldin bizning rasmiy telegram sahifamizga va homiy telegram kanaliga obuna bo'ling. Soʻng botni qayta boshlashni unutmang!",reply_markup=keyboards.getChannelButton())
 
 
 bot.add_custom_filter(IsSubscribed())
