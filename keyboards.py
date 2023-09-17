4d8afc8932709278f129b992991dec6a37cead2e
Endie <a-abdusattorov@outlook.com>
2023-09-17 16:41:02 +0500

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -28,7 +28,6 @@ class Keyboards:
     def getChannelButton(self):
         markup=InlineKeyboardMarkup()
         markup.add(InlineKeyboardButton("coDesign","https://t.me/codes1gn"),row_width=1)
-        markup.add(InlineKeyboardButton("milli ai","https://t.me/milliai"),row_width=1)
         markup.add(InlineKeyboardButton("Tekshirish 🔁",callback_data="check"),row_width=1)
         return markup
     def anotherFilter(self,m):
