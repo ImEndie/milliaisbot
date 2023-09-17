dcb3ccedf89d7fd46e261ccd79fbd6942c96dfe5
Endie <a-abdusattorov@outlook.com>
2023-09-17 16:52:12 +0500

    Update filters.py

--- filters.py
+++ filters.py
@@ -7,9 +7,9 @@ class IsSubscribed(telebot.custom_filters.SimpleCustomFilter):
     def check(message):
         subscribed1 = bot.get_chat_member("@codes1gn",message.from_user.id).status in ['administrator','creator','member'] 
         try:
-            return (subscribed1 and subscribed2) or message.chat.type!='private'
+            return (subscribed1 ) or message.chat.type!='private'
         except:
-            return (subscribed1 and subscribed2) or message.message.chat.type!='private'
+            return (subscribed1 ) or message.message.chat.type!='private'
 class IsAdmin(telebot.custom_filters.SimpleCustomFilter):
     key='is_admin'
     @staticmethod
