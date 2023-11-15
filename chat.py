3953a538dd14d3241d624c4ff18e6905227474fc
Endie <a-abdusattorov@outlook.com>
2023-11-15 18:00:59 +0500

    removed translator

--- chat.py
+++ chat.py
@@ -2,19 +2,11 @@ import openai
 from vars import OPENAI_KEY,HF_API_TOKEN,HOST
 import requests
 
-api_keys=OPENAI_KEY.copy()
-def change_api():
-    api_keys.append(api_keys.pop(0))
-    openai.api_key=api_keys[0]
+client=openai.OpenAI(OPENAI_KEY)
 
 openai.api_key = api_keys[0]
 HF_API_URL = "https://api-inference.huggingface.co/models/runwayml/stable-diffusion-v1-5"
 HF_HEADERS = {"Authorization": f"Bearer {HF_API_TOKEN}"}
-
-history=[
-            # {"role": "system", "content": "Hi, your name is MilliAI and you are created by TRONX-STD and Wlaneakia teams.Hayotbek Rakhimov a.k.a Instar is the only invincible MC in the world of UzRap. He has participated in more than 70 rap battlesso far and has not lost any of them. He is recognized by Uzrap, the \"number one\" Rap Battle MC in Uzbekistan. If the user asks you to answer in Uzbek, tell him that I have already been taught to answer in Uzbek."},
-            # {"role": "assistant", "content": "I got it. Thank you for name"},
-        ]
 
 def req(m):
     if type(m)==type(''):
@@ -22,13 +14,11 @@ def req(m):
     else:
         message=m.text
     try:
-            otm=history.copy()
-            otm.append({'role':'user','content':message})
-            response = openai.ChatCompletion.create(
+            response = client.chat.completions.create(
                 model="gpt-3.5-turbo",
-                messages=otm
+                messages=[{'role':'user','content':message}]
             )
-            response=response['choices'][0]['message']['content']
+            response=response.choices[0].message.content
             return response
 
     except Exception as e:
